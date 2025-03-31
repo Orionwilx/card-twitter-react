3679a9cad0f43dd5d7ef698f91e0a0aafd5ad4ff
Orion <wilman3d@hotmail.com>
2025-03-31 18:57:17 -0500

    initialize React project with Vite setup and basic components

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Twitter Card</h1>
+    </div>
+  );
+}
+
+export default App;
+// This is a simple React component that renders a heading "Twitter Card" inside a div.
