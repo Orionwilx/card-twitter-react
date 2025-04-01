e72a5894ad21ff2b2547cebbb3ce8fe52ca225ba
Orion <wilman3d@hotmail.com>
2025-04-01 18:22:27 -0500

    enhance TwitterFollowCard with state management and default props

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,38 @@ import "./index.css";
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 function App() {
-  const formatUserName = (userName) => `@${userName}`;
+  const formatUserNamef = (userName) => `@${userName}`;
+
+  const orionwil = {
+    initialIsFollowing: false,
+    userName: "orionwilx",
+    name: "Wilman Zuleta",
+  };
+
   return (
     <section className="App">
+      <h2 className="info">A quien quieres seguir ?</h2>
+      {/* Pasar props todas al mimos tiempo soponiendo que vienen de un obajeto */}
+      <TwitterFollowCard formatUserName={formatUserNamef} {...orionwil} />
+
+      {/* Pasar props de forma individual */}
       <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing={false}
-        userName="orionwilx"
-        name="Wilman Zuleta"
-      />
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
+        formatUserName={formatUserNamef}
+        initialIsFollowing
         userName="Caro-ov"
         name="Adrina Olivares"
       />
+
+      {/* Props por default */}
       <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
-        userName="orionwilx"
-        name="Wilman Zuleta"
+        formatUserName={formatUserNamef}
+        initialIsFollowing={false}
       />
+
+      {/* Prop children */}
+      {/* <TwitterFollowCard formatUserName={formatUserNamef}>
+        <h1>Hola mundo</h1>
+      </TwitterFollowCard> */}
     </section>
   );
 }
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 export function TwitterFollowCard({
-  name,
-  userName,
-  isFollowing,
+  name = "unknown",
+  userName = "unknown",
   formatUserName,
+  initialIsFollowing,
+  children,
 }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+  // ===
+  // const state = useState(false);
+  // const isFollowing = state[0];
+  // const setIsFollowing = state[1];
+
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
+  const buttonClassName = isFollowing
+    ? "tw-follow-card-button is-following"
+    : "tw-follow-card-button";
+
   console.log(isFollowing);
   return (
     <article className="tw-follow-card">
+      {/* Esto es el ejemplo por si quieres usar la prop children*/}
+      {children}
       <header className="tw-follow-card-header">
         <img
           src={`https://github.com/${userName}.png`}
@@ -20,9 +42,11 @@ export function TwitterFollowCard({
           </span>
         </div>
       </header>
-
       <aside>
-        <button className="tw-follow-card-button">Seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className="tw-follow-card-text"> {text} </span>
+          <span className="tw-follow-card-stopFollow">Dejar de seguir</span>
+        </button>
       </aside>
     </article>
   );
