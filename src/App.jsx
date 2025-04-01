bf968d8f23d1779c25fef9e2715e1ec2a4d8b4f4
Orion <wilman3d@hotmail.com>
2025-03-31 19:52:22 -0500

    add Twitter follow card component with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import "./index.css";
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <h1>Twitter Card</h1>
-    </div>
+    <article className="tw-follow-card">
+      <header className="tw-follow-card-header">
+        <img
+          src="https://github.com/orionwilx.png"
+          alt="Imagen de perfil"
+          className="tw-follow-card-avatar"
+        />
+        <div className="tw-follow-card-info">
+          <strong>Wilman Zuleta</strong>
+          <span className="tw-follow-card-infoUserName">@Orionwilx</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className="tw-follow-card-button">Seguir</button>
+      </aside>
+    </article>
   );
 }
 
